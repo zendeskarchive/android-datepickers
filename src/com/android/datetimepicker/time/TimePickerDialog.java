991f6f4f972652828dcb78c5dd51ce2605b53d50
Jerzy Chalupski <jerzy@getbase.com>
2014-04-08 10:53:28 +0200

    Use support-v4 DialogFragment class

--- src/com/android/datetimepicker/time/TimePickerDialog.java
+++ src/com/android/datetimepicker/time/TimePickerDialog.java
@@ -18,11 +18,11 @@ package com.android.datetimepicker.time;
 
 import android.animation.ObjectAnimator;
 import android.app.ActionBar.LayoutParams;
-import android.app.DialogFragment;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.KeyCharacterMap;
 import android.view.KeyEvent;
